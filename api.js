f39a31384e0396601ef469e8d724052fa2ed17cd
nicochabmelo@gmail.com <82840508+NicoChabot@users.noreply.github.com>
2021-06-18 02:33:35 -0300

    commit

---
+++ api.js
@@ -0,0 +1,47 @@
+const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
+const APIKEY = '6ca16e3d0799d76ccd0782685dba8f89';
+const IMG_PREFIX = 'https://image.tmdb.org/t/p/w500';
+let xhr;
+$.ajax({
+    url: TMDB_ENDPOINT + '/movie/now_playing',
+    data: {
+        api_key: '6ca16e3d0799d76ccd0782685dba8f89'
+    }
+})
+
+.done(function data() {
+    let codigoHTML = '';
+    for (let i = 0; i < data.results.length; i++) {
+        let nomeFilme = data.results[i].title;
+        let sinopse = data.results[i].overview;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
+
+        textoHTML += `<div class="card col-md-2">
+                <img src="${imagem}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${nomeFilme}</h5>
+                    <p class="card-text">${sinopse}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>`;
+        $('#listafilmes').html(codigoHTML);
+
+    }
+});
+$(document).ready(function() {
+    mostrarfilmes()
+    $('#btnbusca').click(pesquisaFilmes)
+})
+
+function pesquisaFilmes() {
+    xhr = new XMLHttpRequest();
+
+    query = document.getElementById('pesquisa').value;
+
+    xhr.open('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
+    xhr.onload = mostraFilmes();
+    xhr.send();
+}
+
+
+document.getElementById('tela').innerHTML = textoHTML
