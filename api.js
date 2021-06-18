2213ef072acbf76c8e2f2b3a629dd6f9e45a9915
nicochabmelo@gmail.com <82840508+NicoChabot@users.noreply.github.com>
2021-06-18 03:37:39 -0300

    commit

--- api.js
+++ api.js
@@ -1,54 +1,46 @@
 const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
-const APIKEY = '6ca16e3d0799d76ccd0782685dba8f89';
+const APIKEY = '9c2c75d9d67c3f98cc98bb29450115c5';
 const IMG_PREFIX = 'https://image.tmdb.org/t/p/w500';
 let xhr;
 
-function mostrarfilmes() {
+function carregaFilmes() {
     xhr = new XMLHttpRequest();
-    xhr.open('GET', TMDB_ENDPOINT + '/movie/popular' + '?api_key=' + APIKEY, true);
+
+    xhr.open('GET', TMDB_ENDPOINT + '/movie/now_playing' + '?api_key=' + APIKEY, true);
     xhr.onload = exibeFilmes;
     xhr.send();
-    $.ajax({
-        url: TMDB_ENDPOINT + '/movie/now_playing',
-        data: {
-            api_key: '6ca16e3d0799d76ccd0782685dba8f89'
-        }
-    })
+}
 
-    .done(function data() {
-        let codigoHTML = '';
-        for (let i = 0; i < data.results.length; i++) {
-            let nomeFilme = data.results[i].title;
-            let sinopse = data.results[i].overview;
-            let imagem = IMG_PREFIX + data.results[i].poster_path;
+function pesquisaFilmes() {
+    xhr = new XMLHttpRequest();
 
-            textoHTML += `<div class="card col-md-2">
-                <img src="${imagem}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${nomeFilme}</h5>
-                    <p class="card-text">${sinopse}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>`;
-            $('#listafilmes').html(codigoHTML);
+    query = document.getElementById('pesquisa').value;
 
-    document.getElementById('tela').innerHTML = textoHTML
+    xhr.open('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
+}
 
-        }
-    });
-    $(document).ready(function() {
-        mostrarfilmes()
-        $('#btnbusca').click(pesquisaFilmes)
-    })
 
-    function pesquisaFilmes() {
-        xhr = new XMLHttpRequest();
+function exibeFilmes() {
 
-        query = document.getElementById('pesquisa').value;
+    let data = JSON.parse(xhr.responseText);
+    let textoHTML = '';
 
-        xhr.open('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
-        xhr.onload = mostraFilmes();
-        xhr.send();
+    for (let i = 0; i < data.results.length; i++) {
+        let tituloBR = data.results[i].title;
+        let resumao = data.results[i].overview;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
+
+        textoHTML += `<div class="card col-md-4">
+            <img src="${imagem}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${tituloBR}</h5>
+                <p class="card-text">${resumao}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`
     }
 
+    document.getElementById('tela').innerHTML = textoHTML;
 }
